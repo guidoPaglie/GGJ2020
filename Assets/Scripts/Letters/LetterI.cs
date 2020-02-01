d13750818743d7de6a72fab541f52075281766ac
Guido Pagliettini <guido.pagliettini@gmail.com>
2020-02-01 14:01:35 -0300

    Empty letter A

---
+++ Assets/Scripts/Letters/LetterI.cs
@@ -0,0 +1,11 @@
+namespace Letters
+{
+    public class LetterI : Letter
+    {
+        // TODO still don't know what to do...
+        private void OnMouseDown()
+        {
+            _letterRepaired();
+        }
+    }
+}
