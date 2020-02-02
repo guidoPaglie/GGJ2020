43d4a97019c1a127bc156090603a2f79f6ae4aba
Guido Pagliettini <guido.pagliettini@gmail.com>
2020-02-02 13:56:48 -0300

    Letter R better movement

--- Assets/Scripts/Letters/LetterR.cs
+++ Assets/Scripts/Letters/LetterR.cs
@@ -1,15 +1,19 @@
 using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using Random = UnityEngine.Random;
 
 namespace Letters
 {
-    public class LetterR : Letter
+    public class LetterR : Letter, IPointerUpHandler, IPointerDownHandler
     {
         [SerializeField] private float _minDistanceToRepair;
-        [SerializeField] private float _offsetX;
-        [SerializeField] private float _offsetY;
-        
+        [SerializeField] private Rigidbody2D _rigidbody;
+
+        private Vector3 delta;
+        private Vector3 lastPos;
+
+        private bool _clicked;
         private bool _finished;
         
         public override void Break(Action letterRepaired)
@@ -42,13 +46,73 @@ namespace Letters
             transform.position = Camera.main.ScreenToWorldPoint(newPosition);
         }
 
+        
+        void FixedUpdate()
+        {
+            if (!_clicked || _finished) return;
+
+            delta = Input.mousePosition - lastPos;
+
+            var mousePositionX = Input.mousePosition.x;
+            var mousePositionY = Input.mousePosition.y;
+
+            var mousePosWorld = Camera.main.ScreenToWorldPoint(new Vector3(mousePositionX, mousePositionY, 10));
+            var direction = (mousePosWorld - transform.position) * delta.magnitude;
+            _rigidbody.AddForce(direction);
+
+            if (delta.magnitude <= 0.0f)
+            {
+                _rigidbody.velocity = Vector3.zero;
+            }
+        
+            lastPos = Input.mousePosition;
+            
+            if (Vector3.Distance(transform.position, _initialPosition) <= _minDistanceToRepair)
+                Finished();
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            lastPos = eventData.position;
+
+            _clicked = true;
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            if (_finished)
+                return;
+            
+            ResetRigidbody();
+
+            _clicked = false;
+            CalculateNewRandomPosition();
+        }
+
+        private void Finished()
+        {
+            _finished = true;
+            ResetRigidbody();
+
+            _particleSystemFix.SetActive(true);
+            _letterRepaired();
+        }
+
+        private void ResetRigidbody()
+        {
+            _rigidbody.velocity = Vector3.zero;
+            _rigidbody.inertia = 0;
+            _rigidbody.angularVelocity = 0;
+        }
+
+        /*   
         private void OnMouseDrag()
         {
             if (_finished)
                 return;
 
-            var mousePositionX = Input.mousePosition.x - _offsetX;
-            var mousePositionY = Input.mousePosition.y - _offsetY;
+            var mousePositionX = Input.mousePosition.x;
+            var mousePositionY = Input.mousePosition.y;
             transform.position = Camera.main.ScreenToWorldPoint(new Vector3(mousePositionX, mousePositionY, 10));
 
             if (Vector3.Distance(transform.position, _initialPosition) <= _minDistanceToRepair)
@@ -61,14 +125,6 @@ namespace Letters
                 return;
             
             CalculateNewRandomPosition();
-        }
-
-        private void Finished()
-        {
-            _finished = true;
-
-            _particleSystemFix.SetActive(true);
-            _letterRepaired();
-        }
+        }*/
     }
 }
---
+++ Assets/TestRigidbody/TestA.cs
@@ -0,0 +1,50 @@
+﻿using System.Net.Mail;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TestA : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    public Rigidbody2D _rigidbody;
+    public Collider2D _collider2D;
+
+    private Vector3 delta;
+    private Vector3 lastPos;
+
+    private bool _clicked = false;
+
+    void FixedUpdate()
+    {
+        if (!_clicked) return;
+
+        delta = Input.mousePosition - lastPos;
+
+        var mousePositionX = Input.mousePosition.x;
+        var mousePositionY = Input.mousePosition.y;
+
+        var mousePosWorld = Camera.main.ScreenToWorldPoint(new Vector3(mousePositionX, mousePositionY, 10));
+        var direction = (mousePosWorld - transform.position) * delta.magnitude;
+        _rigidbody.AddForce(direction);
+
+        if (delta.magnitude <= 0.0f)
+        {
+            _rigidbody.velocity = Vector3.zero;
+        }
+        
+        lastPos = Input.mousePosition;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        lastPos = eventData.position;
+
+        _clicked = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        _rigidbody.velocity = Vector3.zero;
+        _rigidbody.inertia = 0;
+
+        _clicked = false;
+    }
+}
